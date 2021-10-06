9c75ea850357c95a46e2ace79abdc4b7df3e28d0
The man <ThisIsntTheWay@users.noreply.github.com>
2021-10-06 21:00:40 +0200

    Bootstrap Spring

---
+++ src/main/java/klopfen/rocks/fwdelivery/FwDeliveryApplication.java
@@ -0,0 +1,13 @@
+package klopfen.rocks.fwdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/klopfen/rocks/fwdelivery/FwDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package klopfen.rocks.fwdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
