459c0400a89491ef7a81e9a522f98fc2843291b2
The Man <ThisIsntTheWay@users.noreply.github.com>
2022-03-03 22:54:51 +0100

    [WIP] Configurator, webserver

---
+++ modules/config.rb
@@ -0,0 +1,23 @@
+require 'json'
+
+class Configurator
+    def initialize(targetFile)
+        @@jsonFileName = targetFile
+        raise "Config file '#{targetFile}' not found." if !File.file?(@@jsonFileName)
+    end
+
+    def parse
+        @jsonFile = File.read(@@jsonFileName)
+        @@jsonData = JSON.parse(@jsonFile)
+
+        @@listenPort = @@jsonData[:ListenPort] ? @@jsonData[:ListenPort] : 80
+    end
+
+    def JsonData
+        @@jsonData
+    end
+
+    def ListenPort
+        @@listenPort
+    end
+end
